980baa0b79613d21ec9261bc79099e978981192c
Alekh Bhardwaj <guestalekh@gmail.com>
2024-12-31 17:22:44 +0530

    Env Changesit

--- server.js
+++ server.js
@@ -8,13 +8,27 @@ const port = 3000;
 // Middleware to serve static files
 app.use(express.static('public'));
 
-const clientId = '9679576547cf46a295d7a3a4d8aabe31';
-const clientSecret = '76624e353e5c486aba83da1f87c40d8d';
+const clientId = process.env.CLIENT_ID; 
+const clientSecret = process.env.CLIENT_SECRET;
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: 'C:\\Users\\Alekh\\SongOfTheYear\\songoftheyear-d44bc7cccd62.json',
+    credentials: {
+        "type": "service_account",
+        "project_id": "songoftheyear",
+        "private_key_id": "d44bc7cccd62f6fc68a24e110fff25864ab3df9d",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQC2plqtxTuf6ZSm\nMOuePE0vpa/SaMJ68OBd9VgkcUqz4rOz8+/tw7snOBYglc32kIaDQwlHUhuPaZkr\nGCuD1eaaHd4jQzDgt2oG/YyT+jQ8NT3XYVzTw4huG2L0n1BnB1jvPFOlWRKUJwwq\newZ3RjFLuVHsjVUOPo+XzUAj8b5blVvRgqopXZAScAmF1rVu9Xo1PSg3GeCnt/uB\n18RLPmooFTjDNmDrAlO95sYngOVbyWBTJoTCqp8TP6ZLeEGm6uPnFKscDMTAvKeC\nIndT5fNsE7GYtDGOKVBWGKuTxFGNkx0DuRrqRaUs9aOumQYzvp9IMMukbzcRLB33\nrRYR5nBZAgMBAAECggEABS4nLL4HEKREk8kX6as+qi8KE07m0ytRRcDvYMs4yezQ\nWXZ989GlFXntALwW9RsIwaDNWtE8mF7vme4Lz+epbG4T7o6ZjIoPLwcSbWD/icSd\nziXEBvqI/Ex5DESXeHCYKGLe4XXrBbosyWinWIssW0A0j1+7zZVfJ7hJzSsuUbUY\ntvQxIYg3AFGpw+jp4gOQUm69pcoV07YT9Z8E+zWehxTfgG+bl++IliciIgnR+1p7\nGbqfiu1Cq2xklWBXzXomNg7S9TKH+wri9cNHQgh9tp4cGUKPmVLP/UtLCj27bZE2\niSdTgiPGFgAnltgYLwtU4BUGCQf8ZbZ8kdA9vQ1zLQKBgQDj1tyNgn2SM8iAQOxE\nBLuzJEMzZVKL9NBebdlvJb5+Q8BdYXTLbQDMmTsuTywPuNiSL2kg5bue4myuxlyf\nn2GBzvpwbFII9zU3f+rY9KFM4+pXMTcq8yAySx5VQcWT2xn56OlP3SyidfZwAP36\neCpXHUmwsun+/neBEkZA9tnONQKBgQDNOaMsbNgi+4Ugli87VSkrb3LM+k6hrouF\nBG7pCKHN1vxSTQwxh+eh1iuJXC1N2SQvCTshv0fSRJRM1tMYU8ZwbHSTctI51Ojp\n147U1a3XB/8cK2rdgpEdM6v1flcRzp5ya13WEvJY3BTHN+SE8qIrQb6zXX7TF4Hs\nxVZd7m8uFQKBgQCtQ+XAG99Awu6UUksawvq3FjmgqUooMwoO9bzoIK0IOp/co9N9\nLCzyawktg3n/jtPr//AT24FeJ9T86M7heQRfAjdELawJM24eAcJoMUlFPwIkQsIw\ngGoYtTWhIn6NnY7YlX5whMN/3POHY8nBo4aXbyduH/GBQKVC29Hfb5pdjQKBgQCJ\nLp4EdD2GJNs4jSvFXYfHoRf8LNWi/ioFIJGC9GI0ViEUYqxj75TGeYNgrl0aTKwD\nRQhh5pr70xvOwUsaV+WuANFsPrSIM+n9Zjnzy2II+y9shRlKOJ0ICCJKV8mb/aGs\nmlh/YOreuz6obJQ0ynvcrnXrL/Q25t/ShZbV8ywegQKBgQCiJRlqXSqw8KCYz4k4\n1pbJs3vDbTx0t+hwgpq9M2Q0+tULn6JpusJq1gTfA8QZnegHcwn03aZLUFUqgkN+\nYsXUtRsblTU8WIKOdhHDgwpVBdf7V0ZgtETVLA2+NnomJ9GkvF5TspCPimx0a1b2\n46KoteKOyGu5pQpy7RCg90FEog==\n-----END PRIVATE KEY-----\n",
+        "client_email": "service-account@songoftheyear.iam.gserviceaccount.com",
+        "client_id": "114403551100305471167",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/service-account%40songoftheyear.iam.gserviceaccount.com",
+        "universe_domain": "googleapis.com"
+      }
+      ,
     scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
 });
+
 
 app.get('/token', async (req, res) => {
     const tokenUrl = 'https://accounts.spotify.com/api/token';
@@ -39,7 +53,7 @@ app.get('/token', async (req, res) => {
 app.get('/songs', async (req, res) => {
     const authClient = await auth.getClient();
     const request = {
-        spreadsheetId: '1ZTR0oHd45ovU4EaAtj5dKQcs9qsCdNg_bBQCKnhwV2w',
+        spreadsheetId: process.env.SPREADSHEET_ID,
         range: 'Sheet1!A1:A70',
         auth: authClient,
     };
