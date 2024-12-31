ca6e6c39978c5367579e51b4b8a77d3e4c1e2b88
Alekh Bhardwaj <guestalekh@gmail.com>
2025-01-01 00:45:05 +0530

    huh

--- api/spotify/[url].js
+++ api/spotify/[url].js
@@ -1,36 +1,37 @@
-// Use dynamic import to load node-fetch
-let fetch;
-try {
-    fetch = (await import('node-fetch')).default;
-} catch (error) {
-    console.error('Failed to import node-fetch:', error);
-    throw error;
+import got from 'got';
+
+async function getToken() {
+    const tokenUrl = 'https://accounts.spotify.com/api/token';
+    const authOptions = {
+        headers: {
+            Authorization: 'Basic ' + Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64'),
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'grant_type=client_credentials',
+    };
+
+    try {
+        const response = await got.post(tokenUrl, authOptions).json();
+        return response.access_token;
+    } catch (error) {
+        console.error('Error fetching token:', error);
+        throw error;
+    }
 }
 
 export default async function handler(req, res) {
     const { url } = req.query;
     const songId = url.split('/').pop();
 
-    let fetch;
-    try {
-        fetch = (await import('node-fetch')).default; // Dynamically import node-fetch
-    } catch (error) {
-        console.error('Failed to import node-fetch:', error);
-        res.status(500).json({ error: 'Failed to import node-fetch' });
-        return;
-    }
-
     try {
         const token = await getToken();
-        const response = await fetch(`https://api.spotify.com/v1/tracks/${songId}`, {
-            method: 'GET',
+        const response = await got.get(`https://api.spotify.com/v1/tracks/${songId}`, {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
-        });
-        const data = await response.json();
+        }).json();
         res.status(200).json({
-            thumbnailUrl: data.album.images[0].url,
+            thumbnailUrl: response.album.images[0].url,
             spotifyUrl: `https://open.spotify.com/track/${songId}`,
         });
     } catch (error) {
