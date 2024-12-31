255c925c8068149b33ff251792d33795ddae7a10
Alekh Bhardwaj <guestalekh@gmail.com>
2024-12-31 16:50:26 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const axios = require('axios');
+const { google } = require('googleapis');
+const sheets = google.sheets('v4');
+const app = express();
+const port = 3000;
+
+// Middleware to serve static files
+app.use(express.static('public'));
+
+const clientId = '9679576547cf46a295d7a3a4d8aabe31';
+const clientSecret = '76624e353e5c486aba83da1f87c40d8d';
+
+const auth = new google.auth.GoogleAuth({
+    keyFile: 'C:\\Users\\Alekh\\SongOfTheYear\\songoftheyear-d44bc7cccd62.json',
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+});
+
+app.get('/token', async (req, res) => {
+    const tokenUrl = 'https://accounts.spotify.com/api/token';
+    const authOptions = {
+        method: 'post',
+        url: tokenUrl,
+        headers: {
+            'Authorization': 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64')),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: 'grant_type=client_credentials'
+    };
+
+    try {
+        const response = await axios(authOptions);
+        res.send(response.data.access_token);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+app.get('/songs', async (req, res) => {
+    const authClient = await auth.getClient();
+    const request = {
+        spreadsheetId: '1ZTR0oHd45ovU4EaAtj5dKQcs9qsCdNg_bBQCKnhwV2w',
+        range: 'Sheet1!A1:A70',
+        auth: authClient,
+    };
+
+    try {
+        const response = await sheets.spreadsheets.values.get(request);
+        const songs = response.data.values.flat(); // Flatten the array of arrays to a single array
+        res.send(songs);
+    } catch (error) {
+        console.error('Error fetching songs:', error);
+        res.send([]);
+    }
+});
+
+app.get('/spotify/:url', async (req, res) => {
+    const spotifyUrl = req.params.url;
+    const songId = spotifyUrl.split('/').pop(); // Extracts the song ID from the URL
+    const token = await getToken();
+
+    const options = {
+        method: 'get',
+        url: `https://api.spotify.com/v1/tracks/${songId}`,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    };
+
+    try {
+        const response = await axios(options);
+        res.send({
+            thumbnailUrl: response.data.album.images[0].url,
+            spotifyUrl: spotifyUrl
+        });
+    } catch (error) {
+        console.error('Error fetching song data:', error);
+        res.send(error);
+    }
+});
+
+
+async function getToken() {
+    const tokenUrl = 'https://accounts.spotify.com/api/token';
+    const authOptions = {
+        method: 'post',
+        url: tokenUrl,
+        headers: {
+            'Authorization': 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64')),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: 'grant_type=client_credentials'
+    };
+
+    try {
+        const response = await axios(authOptions);
+        return response.data.access_token;
+    } catch (error) {
+        console.error('Error fetching token:', error);
+        throw error;
+    }
+}
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
